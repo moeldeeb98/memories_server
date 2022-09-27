a662e2b099a89a4ab9a03599743dc28b13ade097
Mohamed A. Eldeeb <meldeeb1998@gmail.com>
2022-09-27 17:24:12 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,26 @@
+import express from "express";
+import bodyParser from "body-parser";
+import cors from "cors";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
+const app = express();
+
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors());
+
+const CONNECTION_URL = process.env.CONNECTION_URL;
+const PORT = process.env.PORT || 5000;
+
+mongoose
+  .connect(CONNECTION_URL)
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`Server running on port: ${PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.log(error.message);
+  });
