c804f01ec7ae3e116a8ab9c07678b7778bad5a3d
Mohamed A. Eldeeb <meldeeb1998@gmail.com>
2022-10-02 17:26:09 +0200

    add create post endpoint

--- index.js
+++ index.js
@@ -4,12 +4,16 @@ import cors from "cors";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 
+import postRoutes from "./routes/posts.js";
+
 dotenv.config();
 const app = express();
 
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
+
+app.use("/posts", postRoutes);
 
 const CONNECTION_URL = process.env.CONNECTION_URL;
 const PORT = process.env.PORT || 5000;
